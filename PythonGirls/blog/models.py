1fda269a8a57fb72cf89de6d5dbf605a24708027
Halú de Cássia <haluanedecassia@gmail.com>
2016-04-07 18:55:07 -0300

    Alteração no banco (arrumando erros)

--- PythonGirls/blog/models.py
+++ PythonGirls/blog/models.py
@@ -1,32 +1,38 @@
-#from __future__ import unicode_literals
+from __future__ import unicode_literals
 
 from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 
+
 class Post(models.Model):
     titulo = models.CharField(max_length=60)
     conteudo = models.TextField()
     data_criacao = models.DateField()
     data_modificacao = models.DateField()
-    #autor_id = models.ForeignKey(Autor)
-    #categoria_id = models.ForeignKey(Categoria)
+    autor_id = models.ForeignKey('blog.Autor')
+    categoria_id = models.ForeignKey('blog.Categoria')
+    tag_id = models.ForeignKey('blog.Tag')
+
 
 class Autor(models.Model):
     user_id = models.OneToOneField(User)
     descricao = models.TextField()
     foto = models.CharField(max_length=255)
 
+
 class Tag(models.Model):
     nome = models.CharField(max_length=60)
     data_criacao = models.DateField()
     data_modificacao = models.DateField()
 
+
 class Categoria(models.Model):
     nome = models.CharField(max_length=60)
     data_criacao = models.DateField()
     data_modificacao = models.DateField()
+
 
 class Banner(models.Model):
     foto = models.CharField(max_length=255)
